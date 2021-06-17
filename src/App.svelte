dda6eeb9e3cec15e661fe94b2248346037133490
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2021-06-17 14:14:21 -0500

    add cdn js

--- src/App.svelte
+++ src/App.svelte
@@ -1,55 +1,47 @@
 <script>
-	import {Router, Link, Route} from "svelte-routing";
-	import Header from './components/Header.svelte'
-	import Footer from './components/Footer.svelte'
-	import Home from './views/Home.svelte'
-import Partners from "./views/Partners.svelte";
-import About from "./views/About.svelte";
-import Questions from "./views/Questions.svelte";
-import Conditions from "./views/Conditions.svelte";
-import Contact from "./views/Contact.svelte";
+  import { Router, Link, Route } from "svelte-routing";
+  import Header from "./components/Header.svelte";
+  import Footer from "./components/Footer.svelte";
+  import Home from "./views/Home.svelte";
+  import Partners from "./views/Partners.svelte";
+  import About from "./views/About.svelte";
+  import Questions from "./views/Questions.svelte";
+  import Conditions from "./views/Conditions.svelte";
+  import Contact from "./views/Contact.svelte";
 </script>
 
+<div class="container-fluid">
+  <Router>
+    <Header />
+    <Route path="/">
+      <Home />
+    </Route>
+    <Route path="/colaboradores">
+      <Partners />
+    </Route>
+    <Route path="/nosotros">
+      <About />
+    </Route>
+    <Route path="/preguntas-frecuentes">
+      <Questions />
+    </Route>
+    <Route path="/terminos-condiciones">
+      <Conditions />
+    </Route>
+    <Route path="/contacto">
+      <Contact />
+    </Route>
+    <Footer />
+  </Router>
+  <button type="button" class="btn btn-primary">Primary</button>
+  <button type="button" class="btn btn-secondary">Secondary</button>
+  <button type="button" class="btn btn-success">Success</button>
+  <button type="button" class="btn btn-danger">Danger</button>
+  <button type="button" class="btn btn-warning">Warning</button>
+  <button type="button" class="btn btn-info">Info</button>
+  <button type="button" class="btn btn-light">Light</button>
+  <button type="button" class="btn btn-dark">Dark</button>
+</div>
 
-
-
-	<div class="container-fluid">
-		
-		<Router>
-			<Header></Header>
-			<Route path="/">
-				<Home />		
-			</Route>
-			<Route path="/colaboradores">
-				<Partners />	
-			</Route>
-			<Route path="/nosotros">
-				<About />	
-			</Route>
-			<Route path="/preguntas-frecuentes">
-				<Questions />	
-			</Route>
-			<Route path="/terminos-condiciones">
-				<Conditions />	
-			</Route>
-			<Route path="/contacto">
-				<Contact />	
-			</Route>
-			<Footer></Footer>
-		</Router>
-		<button type="button" class="btn btn-primary">Primary</button>
-<button type="button" class="btn btn-secondary">Secondary</button>
-<button type="button" class="btn btn-success">Success</button>
-<button type="button" class="btn btn-danger">Danger</button>
-<button type="button" class="btn btn-warning">Warning</button>
-<button type="button" class="btn btn-info">Info</button>
-<button type="button" class="btn btn-light">Light</button>
-<button type="button" class="btn btn-dark">Dark</button>
-	</div>
-
-
-<style >
-
-
-
+<style>
 </style>
--- src/views/About.svelte
+++ src/views/About.svelte
@@ -1,7 +1,38 @@
 <script>
-    
 </script>
 
 <div class="row">
-    hola Nosotros
+  <main>
+    <div class="position-relative overflow-hidden  bg-light">
+      <div class="col-md-5 p-lg-5 mx-auto my-3">
+        <h1 class="display-4 fw-normal">Nosotros</h1>
+
+        <div class="card">
+          <img src="./img/FZ 1.png" class="card-img-top img-fluid" alt="..." />
+          <div class="card-body">
+            <p class=" text-break fs-5 font-monospace">
+              FOOD ZONE, es un software con acceso vía Web, y APP’s de iOS y
+              Android para facilitar la interacción entre usuarios y los
+              establecimientos de la Industria de Alimentos y Bebidas, la
+              Industria de la Hospitalidad, y la Industria del Entretenimiento.
+            </p>
+            <p>
+              Permite al usuario tener una interacción cómoda, segura y completa
+              para su experiencia con restaurantes y lugares de entretenimiento.
+            </p>
+            <p>
+              La plataforma permite hacer pagos con tarjeta de crédito o débito,
+              así como por medio de crédito que se podrá abonar en efectivo al
+              adquirir monederos electrónicos en los distintos puntos de venta.
+            </p>
+          </div>
+        </div>
+        <p class=" fw-normal" />
+      </div>
+      <div class="product-device shadow-sm d-none d-md-block" />
+      <div
+        class="product-device product-device-2 shadow-sm d-none d-md-block"
+      />
+    </div>
+  </main>
 </div>
