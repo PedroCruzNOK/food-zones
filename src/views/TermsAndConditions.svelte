1842c441b6fe922b6047dd0293d1078bfc8c288c
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2021-06-18 14:51:25 -0500

    add animate

--- src/views/TermsAndConditions.svelte
+++ src/views/TermsAndConditions.svelte
@@ -7,12 +7,12 @@
       <div class="col-md-12">
         <div class="row justify-content-center">
           <div class="col-8">
-            <h4 class="display-3 fw-normal" id="teminos-condiciones">Terminos y condiciones</h4>
+            <h4 class="display-3 fw-normal wow animated rubberBand " id="teminos-condiciones">Terminos y condiciones</h4>
             <p>Se necesita la creación de los términos y condiciones.</p>
-            <h4 class="display-3 fw-normal">Aviso de Privacidad</h4>
+            <h4 class="display-3 fw-normal wow animated rubberBand ">Aviso de Privacidad</h4>
             <p>Se necesita la creación del Aviso de Privicadad</p>
 
-            <h4 class="display-3 fw-normal">Política de Protección de Datos</h4>
+            <h4 class="display-3 fw-normal wow animated rubberBand ">Política de Protección de Datos</h4>
             <p>
               Se necesita la creación del apartado Legal, enfocándonos en la
               Política de Protección de Datos.
