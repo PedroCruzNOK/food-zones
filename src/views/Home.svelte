42df70badb47c3a59fd2bb3a55675ddd2fbeeed8
Pedro Cruz <cruzrodriguezpedroj@gmail.com>
2021-06-18 10:54:18 -0500

    ajustes en version de bootstrap

--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -1,10 +1,11 @@
 <script>
-   
-
-    
+    import Slider from "../modules/Slider.svelte";
+    import Aboutus from "../modules/Aboutus.svelte";
+    import Appmobile from "../modules/Appmobile.svelte";
 </script>
 
 <div class="row">
-    
-
+    <Slider />
+    <Aboutus />
+    <Appmobile />
 </div>
