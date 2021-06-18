2dcfbba0de89f0e226e7328b00b531fedf7185bc
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2021-06-18 10:52:39 -0500

    se agregan cambios

--- src/views/About.svelte
+++ src/views/About.svelte
@@ -1,38 +1,66 @@
 <script>
 </script>
 
-<div class="row">
+<div class="row margin-t">
   <main>
-    <div class="position-relative overflow-hidden  bg-light">
-      <div class="col-md-5 p-lg-5 mx-auto my-3">
-        <h1 class="display-4 fw-normal">Nosotros</h1>
+    <div class="position-relative overflow-hidden  bg-light ">
+      <div class="col-md-12">
+        <div class="row justify-content-center">
+          <div class="col-8">
+           <div class="row">
 
-        <div class="card">
-          <img src="./img/FZ 1.png" class="card-img-top img-fluid" alt="..." />
-          <div class="card-body">
-            <p class=" text-break fs-5 font-monospace">
+            <h4 class="display-3 fw-normal wow animated rubberBand" >
+              Nosotros
+            </h4>
+           
+            
+            <img src="./img/FZ 1.png" class="igure-img img-fluid rounded wow zoomIn animated" alt="..." />
+            <p class=" h3 wow rubberBand animated">
               FOOD ZONE, es un software con acceso vía Web, y APP’s de iOS y
               Android para facilitar la interacción entre usuarios y los
               establecimientos de la Industria de Alimentos y Bebidas, la
               Industria de la Hospitalidad, y la Industria del Entretenimiento.
             </p>
-            <p>
+
+            <p class="h3 wow rotateInDownRight animated">
               Permite al usuario tener una interacción cómoda, segura y completa
               para su experiencia con restaurantes y lugares de entretenimiento.
             </p>
-            <p>
+            <p class="h3 wow rotateInDownRight animated">
               La plataforma permite hacer pagos con tarjeta de crédito o débito,
               así como por medio de crédito que se podrá abonar en efectivo al
               adquirir monederos electrónicos en los distintos puntos de venta.
             </p>
+
+
+           </div>
+            
+            <div class="row my-5">
+              <div class="col-sm-12 col-md-6 wow rotateInDownRight animated">
+                <h4>Visión</h4>
+                <p class="h3">
+                  Posicionarnos como el software enfocado en Alimentos y Bebidas
+                  más utilizado en el mercado de México y Estados Unidos.
+                </p>
+              </div>
+              <div class="col-sm-12 col-md-6 wow rotateInUpLeft animated">
+                <h4>Misión</h4>
+                <p class="h3">
+                  Brindar el mejor servicio a cada uno de nuestros clientes
+                  facilitándoles el proceso de compra en establecimientos del
+                  sector de Alimentos y Bebidas.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-        <p class=" fw-normal" />
       </div>
-      <div class="product-device shadow-sm d-none d-md-block" />
-      <div
-        class="product-device product-device-2 shadow-sm d-none d-md-block"
-      />
     </div>
   </main>
 </div>
+
+<style>
+  .margin-t {
+    margin-top: 100px;
+  }
+</style>
--- src/views/FrequentQuestions.svelte
+++ src/views/FrequentQuestions.svelte
@@ -3,15 +3,39 @@
 
 <div class="row margin-t">
   <main>
+
+
     <div class="position-relative overflow-hidden  bg-light">
       <div class="col-md-12">
         <div class="row justify-content-center">
           <div class="col-8">
-            <h4 class="display-3 fw-normal flash" data-animation="flash">
+            <h4 class="display-3 fw-normal wow animated rubberBand" data-animation="flash">
               Preguntas Frecuentes
             </h4>
-            <ul class="list-group list-group-flush">
+            <ul class="list-group list-group-flush wow animated bounceInLeft">
               <li class="list-group-item">
+
+                <p>
+                  <a class="btn btn-primary" data-bs-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Toggle first element</a>
+                  <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Toggle second element</button>
+                  <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target=".multi-collapse" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">Toggle both elements</button>
+                </p>
+                <div class="row">
+                  <div class="col">
+                    <div class="collapse multi-collapse" id="multiCollapseExample1">
+                      <div class="card card-body">
+                        Some placeholder content for the first collapse component of this multi-collapse example. This panel is hidden by default but revealed when the user activates the relevant trigger.
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col">
+                    <div class="collapse multi-collapse" id="multiCollapseExample2">
+                      <div class="card card-body">
+                        Some placeholder content for the second collapse component of this multi-collapse example. This panel is hidden by default but revealed when the user activates the relevant trigger.
+                      </div>
+                    </div>
+                  </div>
+                </div>
                 <p class="fw-bold">1. ¿La app es compatible en Android?</p>
                 <span>
                   Si, la app está disponible tanto en Android como en IOS.
