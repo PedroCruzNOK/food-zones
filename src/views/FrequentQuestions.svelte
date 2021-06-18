4e1ba5564a9c360e146458a06c2ec5ce0b68b640
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2021-06-18 11:17:05 -0500

    add showAnswer

--- src/views/FrequentQuestions.svelte
+++ src/views/FrequentQuestions.svelte
@@ -1,73 +1,65 @@
 <script>
+  const showAnswer = (element) => {
+    if (element.srcElement.nextSibling.nextSibling.style.display == "none") {
+      element.srcElement.nextSibling.nextSibling.style.display = "block";
+    } else {
+      if (element.srcElement.nextSibling.nextSibling.style.display == "block") {
+        element.srcElement.nextSibling.nextSibling.style.display = "none";
+      }
+    }
+  };
 </script>
 
 <div class="row margin-t">
   <main>
-
-
     <div class="position-relative overflow-hidden  bg-light">
       <div class="col-md-12">
         <div class="row justify-content-center">
           <div class="col-8">
-            <h4 class="display-3 fw-normal wow animated rubberBand" data-animation="flash">
+            <h4
+              class="display-3 fw-normal wow animated rubberBand"
+              data-animation="flash"
+            >
               Preguntas Frecuentes
             </h4>
             <ul class="list-group list-group-flush wow animated bounceInLeft">
               <li class="list-group-item">
-
-                <p>
-                  <a class="btn btn-primary" data-bs-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Toggle first element</a>
-                  <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Toggle second element</button>
-                  <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target=".multi-collapse" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">Toggle both elements</button>
+                <p class="fw-bold" on:click={(event) => showAnswer(event)}>
+                  1. ¿La app es compatible en Android?
                 </p>
-                <div class="row">
-                  <div class="col">
-                    <div class="collapse multi-collapse" id="multiCollapseExample1">
-                      <div class="card card-body">
-                        Some placeholder content for the first collapse component of this multi-collapse example. This panel is hidden by default but revealed when the user activates the relevant trigger.
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col">
-                    <div class="collapse multi-collapse" id="multiCollapseExample2">
-                      <div class="card card-body">
-                        Some placeholder content for the second collapse component of this multi-collapse example. This panel is hidden by default but revealed when the user activates the relevant trigger.
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <p class="fw-bold">1. ¿La app es compatible en Android?</p>
-                <span>
+                <span style="display:none;">
                   Si, la app está disponible tanto en Android como en IOS.
                 </span>
               </li>
               <li class="list-group-item">
-                <p class="fw-bold">
+                <p class="fw-bold" on:click={(event) => showAnswer(event)}>
                   2. ¿Cómo puedo crear mi Monedero Electrónico?
                 </p>
-                <span>
+                <span style="display:none;">
                   El monedero electrónico se crea automáticamente al crear una
                   cuenta.
                 </span>
               </li>
               <li class="list-group-item">
-                <p class="fw-bold">
+                <p class="fw-bold" on:click={(event) => showAnswer(event)}>
                   3. ¿En qué países está disponible FoodZone?
                 </p>
-                <span>
+                <span style="display:none;" on:click={(event) => showAnswer(event)}>
                   Por el momento está disponible en Estados Unidos y en México.
                 </span>
               </li>
-              <li class="list-group-item">
+              <li class="list-group-item" on:click={(event) => showAnswer(event)}>
                 <p class="fw-bold">4. ¿Cuánto cuesta descargar la app?</p>
-                <span> La app es totalmente gratuita.</span>
+                <span style="display:none;">
+                  La app es totalmente gratuita.</span
+                >
               </li>
-              <li class="list-group-item">
+              <li class="list-group-item" on:click={(event) => showAnswer(event)}>
                 <p class="fw-bold">
                   5. ¿En dónde puedo utilizar mis puntos del monedero
                   electrónico?
                 </p>
-                <span>
+                <span style="display:none;" on:click={(event) => showAnswer(event)}>
                   En cualquier establecimiento que esté dado de alta con
                   FoodZone.
                 </span>
